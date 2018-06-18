4c4e1b0e8ec56f5e16da9de711162ead8f7eb259
Maria Nunez <mariaalician@gmail.com>
2018-06-18 02:17:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 
-integers = [1,2,3,4,5,6,7,8,9]
-require 'prime'
-def prime?(integers)
+require 'pry'
+def prime?(integer)
+  binding.pry
   integers.each do |integer|
   if integer.prime?
     true
